fe5fc75b05a94acbd8f3b4bf685d52cd73fcd706
Kishan Gajera <me@kgajera.com>
2021-08-01 14:38:22 -0400

    feat: display nested objects using dot notation in vertical table

--- src/util/table.ts
+++ src/util/table.ts
@@ -1,11 +1,21 @@
 import Table from "cli-table";
 import get from "lodash/get";
+import isPlainObject from "lodash/isPlainObject";
 
-type ColumnValue = boolean | null | number | string | undefined;
+type ColumnValue = boolean | null | number | string | TableData | undefined;
+type TableData = { [key: string]: ColumnValue };
 
+/**
+ * Create a table to list all `data` elements
+ *
+ * @param options Options for table; `head` is required and should be a key of
+ * a `data` element
+ * @param data Array of data to be displayed in table
+ * @returns Table instance
+ */
 export function horizontalTable(
   options: { head: string[] },
-  data: { [key: string]: ColumnValue }[]
+  data: TableData[]
 ): Table {
   const table = new Table(options);
 
@@ -22,16 +32,54 @@ export function horizontalTable(
   return table;
 }
 
-export function verticalTable(data: { [key: string]: ColumnValue }): Table {
+/**
+ * Create a vertical table (key/value columns) to be used for a single record
+ *
+ * @param data Data object to be displayed in table
+ * @returns Table instance
+ */
+export function verticalTable(data: TableData): Table {
   const table = new Table();
-  const keys = Object.keys(data);
-  for (const key of keys) {
-    const value = data[key];
-    table.push({ [key]: displayValue(value) });
-  }
+  addVerticalTableRows(table, data);
   return table;
 }
 
-function displayValue(value: ColumnValue) {
-  return value === null || value === undefined ? "" : value;
+/**
+ * Converts column value into a string
+ *
+ * @param value Value to be displayed in table
+ * @returns `value` as a string
+ */
+function displayValue(value: ColumnValue): string {
+  return value === null || value === undefined ? "" : String(value);
+}
+
+/**
+ * Pushes rows to the table for each key in the `data` object. For nested
+ * objects, it will display each nested object field using dot notation.
+ *
+ * @param table Table instance
+ * @param data Object to display in table
+ * @param parentKeys Parent keys for nested objects
+ */
+function addVerticalTableRows(
+  table: Table,
+  data: TableData,
+  parentKeys: string[] = []
+) {
+  const keys = Object.keys(data);
+
+  for (const key of keys) {
+    const value = data[key];
+    const keys = [...parentKeys, key];
+
+    if (isPlainObject(value)) {
+      addVerticalTableRows(table, value as TableData, keys);
+    } else {
+      const stringValue = displayValue(value);
+      if (stringValue.length) {
+        table.push({ [keys.join(".")]: displayValue(value) });
+      }
+    }
+  }
 }
